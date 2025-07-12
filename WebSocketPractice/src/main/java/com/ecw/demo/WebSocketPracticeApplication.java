914fa6e1e4125d0e241cc533863a7d6ddf3be5eb
ashish <ashsoft67@gmail.com>
2025-07-12 11:15:06 +0530

    test commit

--- WebSocketPractice/src/main/java/com/ecw/demo/WebSocketPracticeApplication.java
+++ WebSocketPractice/src/main/java/com/ecw/demo/WebSocketPracticeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebSocketPracticeApplication {
 
 	public static void main(String[] args) {
+		System.out.println("ashish");
 		SpringApplication.run(WebSocketPracticeApplication.class, args);
 	}
 
